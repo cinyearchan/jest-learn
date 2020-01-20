33c89c02fb583c9b7dbf69cd5336d3d10eee3715
cinyearchan <cinyearchan@gmail.com>
2020-01-20 16:17:32 +0800

    axios请求失败处理

--- src/containers/TodoList/__tests__/integration/TodoList.test.js
+++ src/containers/TodoList/__tests__/integration/TodoList.test.js
@@ -2,8 +2,10 @@ import { mount } from '@vue/test-utils'
 import { findTestWrapper } from '../../../../utils/testUtils'
 import TodoList from '../../../TodoList/TodoList.vue'
 import store from '../../../../store'
+import axios from '../../__mocks__/axios'
 
 beforeEach(() => {
+  axios.success = true
   jest.useFakeTimers()
 })
 
@@ -26,12 +28,13 @@ it(`
 it(`
   1. 用户进入页面时，请求远程数据
   2. 列表应该展示远程返回的数据
-`, () => {
+`, (done) => {
   const wrapper = mount(TodoList, { store })
   // 等待异步数据渲染完成
   wrapper.vm.$nextTick(() => {
     const listItems = findTestWrapper(wrapper, 'list-item')
     expect(listItems.length).toBe(2)
+    done()
   })
 })
 
@@ -54,3 +57,16 @@ it(`
   })
 })
 */
+
+it(`
+ 1. 用户进入页面时，请求数据失败
+ 2. 列表应该展示空数据，不能崩溃
+`, (done) => {
+  axios.success = false
+  const wrapper = mount(TodoList, { store })
+  wrapper.vm.$nextTick(() => {
+    const listItems = findTestWrapper(wrapper, 'list-item')
+    expect(listItems.length).toBe(0)
+    done()
+  })
+})
